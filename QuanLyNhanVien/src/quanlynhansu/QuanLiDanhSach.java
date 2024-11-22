1f0e7e1c151ae3a8166afe178bd9cfc3e3223086
hanhut0710 <huynhanhnhut94@gmail.com>
2024-11-22 11:07:59 +0700

    init commit

---
+++ QuanLyNhanVien/src/quanlynhansu/QuanLiDanhSach.java
@@ -0,0 +1,12 @@
+package quanlynhansu;
+
+public interface QuanLiDanhSach {
+	public void them();
+	public void sua();
+	public void xoa();
+	public void timKiem();
+	public void hienThiDanhSach();
+	public void thongKe();
+	public void docFile();
+	public void ghiFile();
+}
