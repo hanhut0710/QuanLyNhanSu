ef1b83cc0f45fcc9feef154eedc7566ff306ce9f
minleo <minhnguyendz2108@gmail.com>
2024-11-26 17:07:02 +0700

    Update PhongIT.java

--- QuanLyNhanSu/src/QuanLyNhanSu/PhongIT.java
+++ QuanLyNhanSu/src/QuanLyNhanSu/PhongIT.java
@@ -8,10 +8,22 @@ public class PhongIT extends PhongBan {
 	@Override
 	public void nhapThongTinPhongBan() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Nhập mã phòng ban : ");
-		this.maPhongBan = sc.nextLine();
 		System.out.println("Nhập tên phòng ban : ");
 		this.tenPhongBan = sc.nextLine();
+		boolean isDuplicate;
+        do {
+            isDuplicate = false;
+            System.out.println("Nhập mã phòng ban: ");
+            this.maPhongBan = sc.nextLine();
+
+            for (PhongBan pb : DanhSachPhongBan.danhSachPhongBan) {
+                if (pb.getMaPhongBan().equals(this.maPhongBan)) {
+                    System.out.println("Mã phòng ban đã tồn tại. Vui lòng nhập lại.");
+                    isDuplicate = true;
+                    break;
+                }
+            }
+        } while (isDuplicate);
 		boolean check = false;
 		while (!check) {
 			boolean found = false;
